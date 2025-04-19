456c09749c6a678f39f1ce500e783c871b6a56f2
Uzair Aslam <uzairaslam990@gmail.com>
2025-04-19 18:19:22 +0500

    business and entertainment pages

--- src/app/(pages)/categories/entertainment/page.js
+++ src/app/(pages)/categories/entertainment/page.js
@@ -1,14 +1,15 @@
-import EntertainmentSection from '@/components/entertainmentSection/EntertainmentSection'
-import React from 'react'
+import EntertainmentSection from "@/components/entertainmentSection/EntertainmentSection";
+import React from "react";
 
 export default function page() {
   return (
-    
     <div className="bg-gray-900 ">
-    <h1 className="text-4xl text-center font-bold py-5 text-gray-200">Entertainment News</h1>
-    <div className="">
-      <EntertainmentSection/>
-   </div>
+      <h1 className="text-4xl text-center font-bold py-5 text-gray-200">
+        Entertainment News
+      </h1>
+      <div className="">
+        <EntertainmentSection />
+      </div>
     </div>
-  )
+  );
 }
