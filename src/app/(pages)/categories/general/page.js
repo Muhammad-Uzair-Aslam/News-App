548a065597ceb6a4478d5276f527b8a403810c2c
Uzair Aslam <uzairaslam990@gmail.com>
2025-04-20 18:20:41 +0500

    general and health pages

--- src/app/(pages)/categories/general/page.js
+++ src/app/(pages)/categories/general/page.js
@@ -1,13 +1,15 @@
-import GeneralSection from '@/components/generalSection/GeneralSection'
-import React from 'react'
+import GeneralSection from "@/components/generalSection/GeneralSection";
+import React from "react";
 
 export default function general() {
   return (
     <div className="bg-gray-900 ">
-      <h1 className="text-4xl text-center font-bold py-5 text-gray-200">General News     </h1>
+      <h1 className="text-4xl text-center font-bold py-5 text-gray-200">
+        General News{" "}
+      </h1>
       <div className="">
-        <GeneralSection/>
-     </div>
+        <GeneralSection />
+      </div>
     </div>
-  )
+  );
 }
